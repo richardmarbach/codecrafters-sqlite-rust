73aeb3c02172a2b5317f554c246e4df88d112cf2
Richard Marbach <rmarbach@gmail.com>
2023-04-06 19:18:14 +0200

    Number of cells in bree index is the number of tables

--- src/main.rs
+++ src/main.rs
@@ -20,11 +20,16 @@ fn main() -> Result<()> {
             file.read_exact(&mut header)?;
 
             // The page size is stored at the 16th byte offset, using 2 bytes in big-endian order
-            #[allow(unused_variables)]
             let page_size = u16::from_be_bytes([header[16], header[17]]);
 
-            // Uncomment this block to pass the first stage
             println!("database page size: {}", page_size);
+
+            let mut btree_header = [0; 12];
+            file.read_exact(&mut btree_header)?;
+
+            let number_of_tables = u16::from_be_bytes([btree_header[3], btree_header[4]]);
+
+            println!("number of tables: {}", number_of_tables);
         }
         _ => bail!("Missing or invalid command passed: {}", command),
     }
