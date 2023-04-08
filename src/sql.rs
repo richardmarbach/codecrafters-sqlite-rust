d5f590b635283b8abe365549d5d6f31eda1ee131
Richard Marbach <rmarbach@gmail.com>
2023-04-08 17:37:16 +0200

    Parse simple select statements

--- src/sql.rs
+++ src/sql.rs
@@ -1,4 +1,5 @@
 use nom::{
+    branch::alt,
     bytes::complete::{tag, tag_no_case, take_while1},
     character::{
         complete::{alphanumeric1, multispace0, multispace1},
@@ -6,7 +7,7 @@ use nom::{
     },
     combinator::{map, opt},
     multi::many1,
-    sequence::{delimited, tuple},
+    sequence::{delimited, terminated, tuple},
     IResult,
 };
 
@@ -34,11 +35,36 @@ pub enum SQLCommand {
 }
 
 pub fn parse(input: &[u8]) -> IResult<&[u8], SQLCommand> {
-    map(creation, |c| SQLCommand::CreateTable(c))(input)
+    alt((
+        map(creation, |c| SQLCommand::CreateTable(c)),
+        map(selection, |s| SQLCommand::Select(s)),
+    ))(input)
+}
+
+fn selection(input: &[u8]) -> IResult<&[u8], SelectStatement> {
+    let (remaining_input, (_, _, fields, _, _, _, table, _)) = tuple((
+        tag_no_case("select"),
+        multispace1,
+        fields,
+        multispace0,
+        tag_no_case("from"),
+        multispace1,
+        identifier,
+        opt(tag(";")),
+    ))(input)?;
+
+    Ok((remaining_input, SelectStatement { table, fields }))
+}
+
+fn fields(input: &[u8]) -> IResult<&[u8], Vec<String>> {
+    many1(terminated(
+        identifier,
+        opt(delimited(multispace0, tag(","), multispace0)),
+    ))(input)
 }
 
 fn creation(input: &[u8]) -> IResult<&[u8], CreateTableStatement> {
-    let (remaining_input, (_, _, _, _, table, _, _, _, fields, _, _)) = tuple((
+    let (remaining_input, (_, _, _, _, table, _, _, _, fields, _, _, _)) = tuple((
         tag_no_case("create"),
         multispace1,
         tag_no_case("table"),
@@ -50,6 +76,7 @@ fn creation(input: &[u8]) -> IResult<&[u8], CreateTableStatement> {
         field_specification_list,
         multispace0,
         tag(")"),
+        opt(tag(";")),
     ))(input)?;
 
     Ok((remaining_input, CreateTableStatement { table, fields }))
@@ -58,6 +85,7 @@ fn creation(input: &[u8]) -> IResult<&[u8], CreateTableStatement> {
 fn identifier(input: &[u8]) -> IResult<&[u8], String> {
     let (input, name) = take_while1(is_alphanumeric)(input)?;
     let name = String::from_utf8(name.to_vec()).unwrap();
+
     Ok((input, name))
 }
 
@@ -80,7 +108,51 @@ mod tests {
     use super::*;
 
     #[test]
-    fn parse_create_table() {
+    fn parse_select_with_one_field() {
+        let input = b"SELECT id FROM test";
+        let (_, result) = parse(input).unwrap();
+
+        assert_eq!(
+            result,
+            SQLCommand::Select(SelectStatement {
+                table: "test".to_string(),
+                fields: vec!["id".to_string()]
+            })
+        );
+    }
+
+    #[test]
+    fn parse_select_with_two_fields() {
+        let input = b"SELECT id, name FROM test";
+        let (_, result) = parse(input).unwrap();
+
+        assert_eq!(
+            result,
+            SQLCommand::Select(SelectStatement {
+                table: "test".to_string(),
+                fields: vec!["id".to_string(), "name".to_string()]
+            })
+        );
+    }
+
+    #[test]
+    fn parse_create_table_with_one_entry() {
+        let input = b"CREATE TABLE test (id INTEGER)";
+        let (_, result) = parse(input).unwrap();
+
+        assert_eq!(
+            result,
+            SQLCommand::CreateTable(CreateTableStatement {
+                table: "test".to_string(),
+                fields: vec![Field {
+                    name: "id".to_string()
+                },]
+            })
+        );
+    }
+
+    #[test]
+    fn parse_create_table_with_two_entries() {
         let input = b"CREATE TABLE test (id INTEGER, name TEXT)";
         let (_, result) = parse(input).unwrap();
 
